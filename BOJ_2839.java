d224de53f2b86c0f12fe97a987e813ae9358b264
Jeon Jongmin <38072189+wakkpu@users.noreply.github.com>
2022-02-15 12:49:23 +0900

    Add files via upload

---
+++ BOJ_2839.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_2839 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int num = 0;
+		while(N >= 0) {
+			if(N%5 == 0) {
+				num += N/5;
+				System.out.println(num);
+				break;
+			} else {
+				N -= 3;
+				num++;
+			}
+			
+			if(N < 0) {
+				System.out.println(-1);
+				break;
+			}
+		}
+	}
+}
---
+++ SW_1210.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class SW_1210 {
+
+	static int [][] map;
+	
+	public static boolean ladder(int x) {
+
+		boolean[][] visited = new boolean[100][100];
+		
+		int ch = 0;
+		int cx = x;
+		while(true) {
+			//System.out.println("start from "+x+" h: "+ch+" x: "+cx);
+			if(ch == 99 && map[ch][cx] == 1) {
+				//System.out.println("꽝");
+				return false;
+			}
+			if(ch == 99 && map[ch][cx] == 2) {
+				//System.out.println("당첨");
+				return true;
+			}
+			
+			if(cx+1 < 100 && map[ch][cx+1] != 0 && visited[ch][cx+1] == false) {
+				//System.out.println("right");
+				visited[ch][cx+1] = true;
+				cx++;
+			}
+			if(cx-1 >= 0 && map[ch][cx-1] != 0 && visited[ch][cx-1] == false) {
+				//System.out.println("left");
+				visited[ch][cx-1] = true;
+				cx--;
+			}
+			if(ch+1 < 100 && map[ch+1][cx] != 0 && visited[ch+1][cx] == false) {
+				//System.out.println("down");
+				visited[ch+1][cx] = true;
+				ch++;
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		map = new int[100][100];
+		
+		for(int t=0; t<10; t++) {
+			st = new StringTokenizer(br.readLine());
+			String T = st.nextToken();
+			
+			for(int i=0; i<100; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j=0; j<100; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			for(int x=0; x<100; x++) {
+				if(map[0][x] == 1) {
+					boolean flag = ladder(x);
+					if(flag == true) {
+						System.out.println("#"+T+" "+x);
+					}
+				}
+			}
+		}
+	}
+}
