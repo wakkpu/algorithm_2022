a90d3c51afaa2263b297228d258c00b568cedc4f
전종민 <ho05248@naver.com>
2022-07-25 00:38:41 +0900

    .

---
+++ BOJ/BOJ_1132.java
@@ -0,0 +1,40 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1132 {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        Map<Character, Integer> map = new HashMap<>();
+        while(N-->0) {
+            String str = br.readLine();
+            for(int i=0; i<str.length(); i++) {
+                if(!map.containsKey(str.charAt(i))) {
+                    map.put(str.charAt(i), 0);
+                }
+                map.put(str.charAt(i), map.get(str.charAt(i))+(int)Math.pow(10, (str.length()-1-i)));
+            }
+        }
+
+        List<Integer> nums = new ArrayList<>();
+        for(Map.Entry<Character, Integer> entry: map.entrySet()) {
+            nums.add(entry.getValue());
+        }
+        nums.sort(Collections.reverseOrder());
+
+        long result = 0;
+        int value = 9;
+
+        for(Integer num: nums) {
+            result += (long) num * (value--);
+        }
+        System.out.println(result);
+    }
+}
