653f154e322c7a5e79a302fa53f0b37f41138d22
Jeon Jongmin <ho0524870@gmail.com>
2022-11-25 16:59:19 +0900

    .

--- BOJ/BOJ_17307.java
+++ BOJ/BOJ_17307.java
@@ -4,71 +4,71 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ_17307 {
-
-    static int N, C;
-    static int[] init;
-    static int[] led;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        init = new int[N];
+        int[] init = new int[N];
         for(int i=0; i<N; i++) {
             init[i] = Integer.parseInt(st.nextToken());
         }
 
-        int minPush = Integer.MAX_VALUE;
-        int minButton = N+1;
+        if(N == 1) {
+            System.out.println("1");
+            System.out.println("0");
+            return;
+        }
 
-        led = new int[N];
-        for(int i=0; i<N; i++) {
-            copy();
-            int push = 0;
-            while(!isEnd()) {
-                pushButton(i);
-                System.out.println((i+1)+" : "+Arrays.toString(led));
-                push++;
-            }
-
-            if(push < minPush) {
-                minButton = i+1;
-                minPush = push;
+        // 숫자 변하는 패턴
+        int[][] switches = new int[C][C];
+        for(int j=0; j<C; j++) {
+            switches[0][j] = j;
+        }
+        for(int i=1; i<C; i++) {
+            switches[i][0] = switches[i-1][C-1];
+            for(int j=1; j<C; j++) {
+                switches[i][j] = switches[i-1][j-1];
             }
         }
 
+        // i가 j와 같은 숫자가 되기 위해 몇 번 바뀌어야 하는지
+        int[][] pushes = new int[N][N];
+        for(int i=0; i<N-1; i++) {
+            pushes[i][i+1] = switches[init[i]][init[i+1]];
+        }
+        for(int i=N-1; i>=1; i--) {
+            pushes[i][i-1] = switches[init[i]][init[i-1]];
+        }
+
+        int minPush = Integer.MAX_VALUE;
+        int minButton = Integer.MAX_VALUE;
+
+        for(int start=0; start<N; start++) {
+
+            int leftPush = 0;
+            int leftR = start;
+            int leftC = start-1;
+            while(leftR >= 0 && leftC >= 0) {
+                leftPush += pushes[leftR--][leftC--];
+            }
+
+            int rightPush = 0;
+            int rightR = start;
+            int rightC = start+1;
+            while(rightR < N && rightC < N) {
+                rightPush += pushes[rightR++][rightC++];
+            }
+
+            if(Math.max(leftPush, rightPush) < minPush) {
+                minButton = start+1;
+                minPush = Math.max(leftPush, rightPush);
+            }
+        }
         System.out.println(minButton);
         System.out.println(minPush);
-    }
-    public static void copy() {
-        for(int i=0; i<N; i++) {
-            led[i] = init[i];
-        }
-    }
-
-    public static boolean isEnd() {
-        for(int i=0; i<N-1; i++) {
-            if(led[i] != led[i+1]) return false;
-        }
-        return true;
-    }
-
-    public static void pushButton(int buttonNumber) {
-        int left = buttonNumber-1;
-        while(left >= 0 && led[left] == led[buttonNumber]) {
-            led[left] = (led[left]+1)%C;
-            left--;
-        }
-
-        int right = buttonNumber+1;
-        while(right < N && led[right] == led[buttonNumber]) {
-            led[right] = (led[right]+1)%C;
-            right++;
-        }
-        led[buttonNumber] = (led[buttonNumber]+1)%C;
     }
 }
